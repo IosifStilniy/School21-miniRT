81bed877d95914c7a63955e49f668ba57dd24e72
Igor Dmitriev <dmitriev.id0@gmail.com>
2022-07-02 20:31:58 +0300

    implemented bonus gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ncarob <ncarob@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 16:45:22 by ncarob            #+#    #+#             */
-/*   Updated: 2021/10/18 22:58:03 by ncarob           ###   ########.fr       */
+/*   Updated: 2022/07/02 20:31:32 by ncarob           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ static void	check_buffer(char *buf, char **result, char **newline_pointer)
 {
 	char	*previous_newline;
 
-	if (buf[0])
+	if (buf && buf[0])
 	{
 		previous_newline = ft_strchr(buf, '\n');
 		*result = add_buffer(*result, previous_newline + 1);
@@ -25,9 +25,44 @@ static void	check_buffer(char *buf, char **result, char **newline_pointer)
 	}
 }
 
+static void	move_buffer(int fd, int *ret, char **buf_fd, char **result)
+{
+	if ((*buf_fd) && buf_fd[0])
+	{
+		free((*buf_fd));
+		(*buf_fd) = NULL;
+	}
+	*buf_fd = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!(*buf_fd))
+	{
+		*ret = 0;
+		return ;
+	}
+	*ret = read(fd, *buf_fd, BUFFER_SIZE);
+	if (*ret < 0)
+		return ;
+	(*buf_fd)[*ret] = 0;
+	*result = add_buffer(*result, (*buf_fd));
+}
+
+static void	*get_rid_of_everything(char **buf, char **result)
+{
+	if (buf && *buf)
+	{
+		free(*buf);
+		*buf = NULL;
+	}
+	if (result && *result)
+	{
+		free(*result);
+		*result = NULL;
+	}
+	return (NULL);
+}
+
 char	*get_next_line(int fd)
 {
-	static char	buf[BUFFER_SIZE + 1];
+	static char	*buf[OPEN_MAX];
 	char		*result;
 	char		*newline_pointer;
 	int			ret;
@@ -35,19 +70,18 @@ char	*get_next_line(int fd)
 	ret = 1;
 	newline_pointer = NULL;
 	result = NULL;
-	check_buffer(buf, &result, &newline_pointer);
+	if (fd < 0 || fd > OPEN_MAX)
+		return (NULL);
+	check_buffer(buf[fd], &result, &newline_pointer);
 	while (ret && !newline_pointer)
 	{
-		ret = read(fd, buf, BUFFER_SIZE);
-		buf[ret] = 0;
-		result = add_buffer(result, buf);
-		if (ret == -1 || (!ret && !result) || (result && !result[0]))
-		{
-			free(result);
-			return (NULL);
-		}
+		move_buffer(fd, &ret, &buf[fd], &result);
+		if (ret < 0 || (!ret && !result) || (result && !result[0]))
+			return (get_rid_of_everything(&buf[fd], &result));
 		newline_pointer = ft_strchr(result, '\n');
 	}
+	if (!newline_pointer)
+		get_rid_of_everything(&buf[fd], NULL);
 	if (newline_pointer)
 		*(newline_pointer + 1) = 0;
 	return (result);
