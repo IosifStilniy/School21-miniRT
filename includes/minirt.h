dcd29e2faa5c2aef529b853a62aaafdfbd73e8d3
desssai <75085822+desssai@users.noreply.github.com>
2022-07-03 23:17:04 +0300

    updated header

--- includes/minirt.h
+++ includes/minirt.h
@@ -6,7 +6,7 @@
 /*   By: ncarob <ncarob@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 14:58:29 by ncarob            #+#    #+#             */
-/*   Updated: 2022/07/02 14:10:48 by ncarob           ###   ########.fr       */
+/*   Updated: 2022/07/03 22:53:51 by ncarob           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@
 # define KEY_C 8
 # define KEY_R 15
 # define KEY_I 34
+# define KEY_N 45
 # define KEY_UP 126
 # define KEY_LEFT 123
 # define KEY_DOWN 125
@@ -83,6 +84,10 @@
 
 # ifndef FRAMECLR
 #  define FRAMECLR 0x00FFFFFF
+# endif
+
+# ifndef NORMCLR
+#  define NORMCLR 0x00FF0000
 # endif
 
 # ifndef CRNRS
@@ -234,10 +239,10 @@ typedef struct s_dots {
 }	t_dots;
 
 typedef struct s_polys {
-	int			polynum;
-	t_data		txtr;
-	t_data		heightmap;
-	t_poly		*poly;
+	int		polynum;
+	t_data	txtr;
+	t_bool	checkerboard;
+	t_poly	*poly;
 }	t_polys;
 
 typedef struct s_obj {
@@ -288,6 +293,7 @@ typedef struct s_keybrd {
 	t_bool	render;
 	t_bool	interface;
 	t_bool	legend;
+	t_bool	normalpaint;
 }	t_keybrd;
 
 typedef struct s_button {
@@ -352,26 +358,23 @@ typedef struct s_import {
 
 // Parsing the file.
 
+char	*avoidonelinevrtxs(t_vrtx vrtxs[3], char *line, t_import *imp);
+void	centroiddefiner(t_cart *dots, int dotsnum);
 void	crdstmdefiner(t_crdstm *crdstm);
 float	cylinderparser(char *str, t_obj *obj, char *prog, void *mlx);
+void	checkvrtxsnorms(t_vrtx vrtxs[3], t_cart *polynorm);
 int		file_check(char *file, char *prog, t_bool scenefile);
 void	ft_read_information(int fd, t_info *info);
 t_cart	*getcart(t_list *v, int indx);
 char	*getfilename(char *start, char *end);
 void	modelparser(int fd, t_import *imp);
+char	*vrtxparser(char *line, t_list *vt, t_list *vn, t_vrtx *vrtx);
 char	*notendedline(char *line);
 float	objparser(char *line, t_obj *obj, char *prog, void *mlx);
 void	planeparser(char *str, t_obj *obj, char *prog, void *mlx);
 int		primitivedefiner(char *str);
 float	sphereparser(char *str, t_obj *obj, char *prog, void *mlx);
-
-// Parsing utilities.
-
-float	ft_atof(const char *num);
-int		ft_clear_char_array(char **array);
-char	*ft_get_color_values(char *str, t_cart *color, char *prog);
-char	*ft_get_position_values(char *prog, char *str, t_cart *pos);
-char	*skipnumnspaces(char *str, t_bool onlyspaces);
+void	txtrparsing(char *str, t_data *txtr, void *mlx, t_bool *checkerboard);
 
 /*
 We can modify atoi so it checks whether the string ends with the number. 
@@ -395,13 +398,6 @@ void	surfing(t_poly *poly, int *dotindxs, t_cart *dots, void *txtr);
 int 	ft_fill_cylinder_info(char **piece, t_info *info);
 int 	ft_fill_sphere_info(char **piece, t_info *info);
 int		ft_fill_plane_info(char **piece, t_info *info);
-
-// Main execution utilities.
-
-int		ft_exit(void);
-int		ft_clear_info(t_info *info);
-int		ft_key_hook(int keycode, t_info *info);
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
 
 // Orientation and movement in space
 
@@ -439,7 +435,9 @@ void	cornerbuilder(t_cart corners[CRNRS], t_res *wincntr, float focus);
 void	createcamobjs(t_list **camobjs, t_list *objs, t_cart **lightpos, int lightcount);
 void	createframerouts(t_list *objs);
 void	initview(t_list *objs, t_camera *camera, t_list *lights);
-void	framepic(t_camera *camera, t_res *wincntr, t_list *camobjs, t_data *img);
+void	framepainter(t_dots *dots, float focus, t_data *img, t_ui color);
+void	framepic(t_win *win, t_bool normalprint, t_list *camobjs, t_data *img);
+void	normpainter(t_obj *obj, float focus, t_data *img);
 t_bool	objinframe(t_obj *obj, t_res *winctr, float focus);
 void	paintline(t_cart src[2], t_ui color, float focus, t_data *img);
 void	planeframing(t_obj *plane, t_camera *camera, t_data *img);
@@ -480,11 +478,26 @@ t_obj	*objcast(t_list *lst);
 
 // MyFUncs
 
-t_ui	ft_create_trgb(int t, int r, int g, int b);
+// Parsing utilities.
+
+float	ft_atof(const char *num);
+int		ft_clear_char_array(char **array);
+char	*ft_get_color_values(char *str, t_cart *color, char *prog);
+char	*ft_get_position_values(char *prog, char *str, t_cart *pos);
+char	*skipnumnspaces(char *str, t_bool onlyspaces);
+
+// Main execution utilities.
+
+int		ft_exit(void);
+int		ft_clear_info(t_info *info);
+int		ft_key_hook(int keycode, t_info *info);
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
+
 int		get_r(int trgb);
 int		get_g(int trgb);
 int		get_b(int trgb);
 void	ft_draw_screen(t_info *info);
+t_ui	ft_create_trgb(int t, int r, int g, int b);
 
 float	ft_max(float a, float b);
 void	ft_vectnorm(t_cart *vector);
@@ -497,7 +510,11 @@ void	ft_multvects(t_cart *vect_a, t_cart *vect_b, t_cart *dest);
 void	ft_summvects(t_cart *vect_a, t_cart *vect_b, t_cart *dest);
 void	ft_crossprod(t_cart *vect_a, t_cart *vect_b, t_cart *dest);
 
+void	ft_checker_poly(t_cart *cuv);
+void	my_mlx_get_pixel(t_data *data, t_cart *color);
 void	ft_cast_ray(t_ray *ray, t_cart *direction, t_cart *origin);
+void	ft_checker_plane(t_cart *ox, t_cart *oy, t_cart *color, t_cart *phit);
+void	ft_texture_plane(t_crdstm *vects, t_cart *color, t_cart *phit, t_data *data);
 void	ft_shadowing(unsigned int *color, t_cart *phit, t_cart norm_colr[2], t_info *info);
 
 float	ft_hit_sphere(t_ray ray, t_obj *sphere);
